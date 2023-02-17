ac6f79ffeac344d57867c145ccec8a093c9aa0fc
Ildar <mexx0174@gmail.com>
2023-02-17 20:46:06 +0500

    1 commit

---
+++ src/main/java/me/mexx/noskiapp/NoskiAppApplication.java
@@ -0,0 +1,13 @@
+package me.mexx.noskiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoskiAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoskiAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/mexx/noskiapp/NoskiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package me.mexx.noskiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoskiAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
